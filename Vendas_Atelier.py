9c3702fac3790d9d5f20f199264e101371d94ab3
mocozairos <mcruzdelirafilho@gmail.com>
2024-09-14 11:57:59 -0300

    Update Vendas_Atelier.py

--- Vendas_Atelier.py
+++ Vendas_Atelier.py
@@ -1091,12 +1091,8 @@ def puxar_bd_receitas():
 
     st.session_state.df_receitas['mes'] = st.session_state.df_receitas['mes'].astype(int)
 
-    st.session_state.df_receitas['valor'] = st.session_state.df_receitas['valor'].replace({'[^\d,.-]': ''}, regex=True)
+    st.session_state.df_receitas['valor'] = st.session_state.df_receitas['valor'].astype(int)
     
-    st.session_state.df_receitas['valor'] = st.session_state.df_receitas['valor'].str.replace(',', '.')
-    
-    st.session_state.df_receitas['valor'] = pd.to_numeric(st.session_state.df_receitas['valor'], errors='coerce')
-
     st.session_state.df_receitas['Ano/Mês'] = st.session_state.df_receitas['mes'].astype(str).str.zfill(2) + '/' + st.session_state.df_receitas['ano'].astype(str).str[-2:]
 
 st.set_page_config(layout='wide')
@@ -1218,10 +1214,6 @@ if data_inicial and data_final and unidade and tipo_produto and noiva_festa:
     row3 = st.columns(1)
 
     if analise=='Vendas vs Qtd.' or analise=='Ticket Médio vs Qtd.' or analise=='Vendas vs Ticket Médio':
-
-        st.session_state.df_receitas
-
-        df_receitas_filtro
 
         df_grafico_mensal_1, df_grafico_mensal_2, df_grafico_trimestral_1, df_grafico_trimestral_2, df_grafico_anual = \
             criar_df_mensal_trimestral_anual(df_receitas_filtro, df_receitas_mesmo_mes, df_receitas_trimestral, df_receitas_mesmo_trimestre, 
