357003adf51800e4fb4d4b79174212570b40dc0c
mocozairos <mcruzdelirafilho@gmail.com>
2024-09-14 11:22:52 -0300

    Update Vendas_Atelier.py

--- Vendas_Atelier.py
+++ Vendas_Atelier.py
@@ -1133,6 +1133,14 @@ if data_inicial and data_final:
     df_trimestre_atual = st.session_state.df_receitas.loc[(st.session_state.df_receitas['ano'] == ano_final) & 
                                                           (st.session_state.df_receitas['mes'] == mes_final), ['trimestre']]
 
+    st.write(st.session_state.df_receitas['ano'].unique().tolist())
+
+    st.write(st.session_state.df_receitas['mes'].unique().tolist())
+    
+    st.write(ano_final)
+
+    st.write(mes_final)
+    
     df_trimestre_atual
 
     trimestre_atual = df_trimestre_atual['trimestre'].iloc[0]
